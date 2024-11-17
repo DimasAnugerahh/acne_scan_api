7c097a9848045dc7f82f86e6aee56f19418b1e22
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-11-17 17:27:45 +0800

    refactor: change article model

--- internal/model/domain/article.go
+++ internal/model/domain/article.go
@@ -3,9 +3,9 @@ package domain
 import "time"
 
 type Article struct {
-	ArticleId   int    `gorm:"type:int;primarykey" json:"article_id"`
-	Image       string `gorm:"type:varchar(255)" json:"image"`
-	Description string `gorm:"type:varchar(512)" json:"description"`
+	ArticleId   int    `json:"article_id"`
+	Image       string `json:"image"`
+	Description string `json:"description"`
 
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
