b25b20ba1368aeb948738f374a8a348b9681bd10
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-11-17 21:32:20 +0800

    feat: new migration for changing data type of created and updated at

---
+++ internal/infrastructure/database/migrations/20241117121746_alter_table_change_date_to_datetime.down.sql
@@ -0,0 +1,17 @@
+ALTER Table users modify COLUMN created_at DATE;
+ALTER Table users modify COLUMN updated_at DATE;
+
+ALTER Table article modify COLUMN created_at DATE;
+ALTER Table article modify COLUMN updated_at DATE;
+
+ALTER Table history modify COLUMN created_at DATE;
+ALTER Table history modify COLUMN updated_at DATE;
+
+ALTER Table ingredients modify COLUMN created_at DATE;
+ALTER Table ingredients modify COLUMN updated_at DATE;
+
+ALTER Table pruduct_recommendation modify COLUMN created_at DATE;
+ALTER Table pruduct_recommendation modify COLUMN updated_at DATE;
+
+
+
