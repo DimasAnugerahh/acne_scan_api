d4d87729e128d131a624d8493fedc4194bef0f50
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-11-17 21:33:37 +0800

    feat: add routes for articles

--- internal/app/articles/routes/article_routes.go
+++ internal/app/articles/routes/article_routes.go
@@ -4,4 +4,6 @@ import "github.com/gofiber/fiber/v2"
 
 func (articleRoutes *ArticleRoutesImpl) ArticleRoutes(app *fiber.App) {
 	app.Post("/articles", articleRoutes.ArticleHandler.Create)
+	app.Get("/articles", articleRoutes.ArticleHandler.GetAll)
+	app.Get("/articles/:id", articleRoutes.ArticleHandler.GetById)
 }
