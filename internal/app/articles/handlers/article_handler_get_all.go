b1e7a4928a7e5c5a9b96fdbd4c4a5b08bbda5051
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-11-17 21:32:45 +0800

    feat: add get all articles

---
+++ internal/app/articles/handlers/article_handler_get_all.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"acne-scan-api/internal/pkg/response"
+	"net/http"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func (articleHandler *ArticleHandlerImpl) GetAll(c *fiber.Ctx) error {
+
+	data, err := articleHandler.ArticleService.GetAll()
+	if err != nil {
+		if strings.Contains(err.Error(), "not found") {
+			return response.BadRequest(c, "category not found", err)
+		}
+		return response.InternalServerError(c, "failed to get all category, something happen", err)
+	}
+
+	return response.StatusOk(c,http.StatusOK,"success",data)
+}
--- internal/app/articles/repository/article_repository_get_all.go
+++ internal/app/articles/repository/article_repository_get_all.go
@@ -1,3 +1,37 @@
 package repository
 
-// func (articleRepository *ArticleRepositoryImpl) GetAll() ([]domain.Article, error)
+import (
+	"acne-scan-api/internal/model/domain"
+	"fmt"
+)
+
+func (articleRepository *ArticleRepositoryImpl) GetAll() ([]domain.Article, error) {
+
+	result := []domain.Article{}
+
+	// rows, err := articleRepository.DB.Query("select article_id, image, description, created_at,updated_at from article")
+
+	rows, err := articleRepository.DB.Query("select article_id, image, description,created_at,updated_at from article")
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		each := domain.Article{}
+		err = rows.Scan(
+			&each.ArticleId,
+			&each.Image,
+			&each.Description,
+			&each.CreatedAt,
+			&each.UpdatedAt,
+		)
+		if err != nil {
+            fmt.Println(err.Error())
+
+			return nil,err
+        }
+		result = append(result, each)
+	}
+
+	return result, nil
+}
