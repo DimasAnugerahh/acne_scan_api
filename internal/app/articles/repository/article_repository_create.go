69f68562a67ad96445cd9e0bf0fca2f182aa6503
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-11-17 21:33:22 +0800

    refactor: add entities that selected

--- internal/app/articles/repository/article_repository_create.go
+++ internal/app/articles/repository/article_repository_create.go
@@ -3,14 +3,14 @@ package repository
 import "acne-scan-api/internal/model/domain"
 
 func (articleRepository *ArticleRepositoryImpl) Create(article *domain.Article) error {
-	stmt, err := articleRepository.DB.Prepare("insert into article (article_id,image,description) values (?,?,?)")
+	stmt, err := articleRepository.DB.Prepare("insert into article (article_id,image,description,created_at,updated_at) values (?,?,?,?,?)")
 	if err != nil {
 		return err
 	}
 
 	defer stmt.Close()
 
-	_, err = stmt.Exec(article.ArticleId, article.Image, article.Description)
+	_, err = stmt.Exec(article.ArticleId, article.Image, article.Description,article.CreatedAt,article.UpdatedAt)
 	if err != nil {
 		return err
 	}
