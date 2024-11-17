ff80ad25259c8d44ea670a9a1bc1b10b1e197a21
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-11-17 21:34:25 +0800

    refactor: add entities that selected

--- internal/model/web/article_request.go
+++ internal/model/web/article_request.go
@@ -1,9 +1,13 @@
 package web
 
+import "time"
+
 type ArticleCreateRequest struct {
 	ArticleId   int    `json:"article_id"`
 	Image       string `json:"image" validate:"required"`
 	Description string `json:"description" validate:"required"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 type ArticleUpdateRequest struct {
