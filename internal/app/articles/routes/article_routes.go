1789a9ddbb0058c6d4f4f7f54767df4ddb8084d1
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-11-18 08:39:31 +0800

    feat: add route func

--- internal/app/articles/routes/article_routes.go
+++ internal/app/articles/routes/article_routes.go
@@ -6,4 +6,7 @@ func (articleRoutes *ArticleRoutesImpl) ArticleRoutes(app *fiber.App) {
 	app.Post("/articles", articleRoutes.ArticleHandler.Create)
 	app.Get("/articles", articleRoutes.ArticleHandler.GetAll)
 	app.Get("/articles/:id", articleRoutes.ArticleHandler.GetById)
+	app.Delete("/articles/:id", articleRoutes.ArticleHandler.Delete)
+	app.Patch("/articles/:id", articleRoutes.ArticleHandler.Update)
+
 }
