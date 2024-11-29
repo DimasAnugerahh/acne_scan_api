bd6611e0974d647d64685af51464538a461aecbb
DimasAnugerahh <112329556+DimasAnugerahh@users.noreply.github.com>
2024-11-29 21:23:44 +0800

    Update setup.go

--- internal/app/setup.go
+++ internal/app/setup.go
@@ -3,16 +3,16 @@ package app
 import (
 	"acne-scan-api/internal/app/articles"
 	productrecommendation "acne-scan-api/internal/app/product_recommendation"
+	cloudstorage "acne-scan-api/internal/pkg/cloud_storage"
 	"database/sql"
 
 	"github.com/go-playground/validator"
 	"github.com/gofiber/fiber/v2"
 )
 
-func InitApp(db *sql.DB,validate *validator.Validate,app *fiber.App){
-	ArticleRoutes := articles.ArticleSetup(db,validate)
-	ProductRecommendationRoutes:=productrecommendation.ProductRecommendationSetup(db,validate)
-
+func InitApp(db *sql.DB, validate *validator.Validate, app *fiber.App, bs cloudstorage.StorageBucketUploader) {
+	ArticleRoutes := articles.ArticleSetup(db, validate, bs)
+	ProductRecommendationRoutes := productrecommendation.ProductRecommendationSetup(db, validate)
 
 	ProductRecommendationRoutes.ProductRecommendationRoutes(app)
 	ArticleRoutes.ArticleRoutes(app)
