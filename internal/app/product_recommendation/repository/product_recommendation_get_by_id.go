bdcb9d9fab6061c1104c344b286dabe950a92de4
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-12-01 18:05:19 +0800

    refactor: change error message

--- internal/app/product_recommendation/repository/product_recommendation_get_by_id.go
+++ internal/app/product_recommendation/repository/product_recommendation_get_by_id.go
@@ -2,24 +2,23 @@ package repository
 
 import (
 	"acne-scan-api/internal/model/domain"
-	"fmt"
 )
 
-func (pr *ProductRecommendationImpl) GetById(id int) (*domain.ProductRecommendation,error) {
+func (pr *ProductRecommendationImpl) GetById(id int) (*domain.ProductRecommendation, error) {
 
-	result:=domain.ProductRecommendation{}
+	result := domain.ProductRecommendation{}
 
-	err := pr.DB.QueryRow("select recommendation_id,image,link,description,created_at,updated_at from pruduct_recommendation where recommendation_id=?",id).Scan(
+	err := pr.DB.QueryRow("select recommendation_id,image,link,description,created_at,updated_at from pruduct_recommendation where recommendation_id=?", id).Scan(
 		&result.RecommendationId,
 		&result.Image,
 		&result.Link,
 		&result.Description,
 		&result.CreatedAt,
 		&result.UpdatedAt,
-		)
+	)
 	if err != nil {
-		return nil,fmt.Errorf("query gagal %s", err.Error())
+		return nil, err
 	}
 
-	return &result,nil
+	return &result, nil
 }
