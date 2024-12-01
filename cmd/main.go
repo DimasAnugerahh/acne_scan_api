6d988f08a5603fc0befc1798bc43f9b1ca3ccbd9
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-12-01 18:59:27 +0800

    refactor: implement go routine

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"acne-scan-api/internal/app"
 	"acne-scan-api/internal/infrastructure/database"
 	cloudstorage "acne-scan-api/internal/pkg/cloud_storage"
+	"database/sql"
+	"sync"
 
 	"github.com/go-playground/validator"
 	"github.com/gofiber/fiber/v2"
@@ -14,28 +16,56 @@ import (
 )
 
 func main() {
+
+	var db *sql.DB
+	// var config
+	var err error
+	var bucketUploader cloudstorage.StorageBucketUploader
+
 	fiber := fiber.New()
 
+	var wg sync.WaitGroup
+	wg.Add(4)
+
+	// go func() {
+	// 	defer wg.Done()
 	config, err := configs.LoadConfig()
 	if err != nil {
 		logrus.Fatal("Error loading config:", err.Error())
 	}
+	// }()
 
 	// Initialize database connection
-	db, err := database.NewMySQLConnection(&config.MySQL)
-	if err != nil {
-		logrus.Fatal("Error connecting to MySQL:", err.Error())
-	}
+	go func() {
+		defer wg.Done()
+		db, err = database.NewMySQLConnection(&config.MySQL)
+		if err != nil {
+			logrus.Fatal("Error connecting to MySQL:", err.Error())
+		}
+	}()
 
 	validate := validator.New()
-	bucketUploader := cloudstorage.NewStorageBucketUploader(&config.StorageBucket)
 
-	app.InitApp(db,validate,fiber,bucketUploader)
+	go func() {
+		defer wg.Done()
+		bucketUploader = cloudstorage.NewStorageBucketUploader(&config.StorageBucket)
+	}()
+
+	go func() {
+		defer wg.Done()
+		app.InitApp(db, validate, fiber, bucketUploader)
+	}()
 
 	fiber.Use(cors.New())
 	fiber.Use(logger.New(logger.Config{
 		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
 	}))
 
-	fiber.Listen(":8080")
+	go func() {
+		defer wg.Done()
+		fiber.Listen(":8080")
+	}()
+	
+	wg.Wait()
+
 }
