f87e660e6e6869e961bfce4b2a1404869cc59bde
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-12-02 10:07:27 +0800

    feat : set form value

--- internal/model/web/product_recommendation_request.go
+++ internal/model/web/product_recommendation_request.go
@@ -3,17 +3,17 @@ package web
 import "time"
 
 type ProductRecommendationRequest struct {
-	RecommendationId int       `json:"recommendation_id" validate:"required"`
-	Image            string    `json:"image" validate:"required"`
-	Link             string    `json:"link" validate:"required"`
-	Description      string    `json:"description" validate:"required"`
-	CreatedAt        time.Time `json:"created_at"`
-	UpdatedAt        time.Time `json:"updated_at"`
+	RecommendationId int       `form:"recommendation_id" json:"recommendation_id" validate:"required"`
+	Image            string    `form:"image" json:"image"`
+	Link             string    `form:"link" json:"link" validate:"required"`
+	Description      string    `form:"description" json:"description" validate:"required"`
+	CreatedAt        time.Time `form:"created_at" json:"created_at"`
+	UpdatedAt        time.Time `form:"updated_at" json:"updated_at"`
 }
 
 type ProductRecommendationUpdateRequest struct {
-	Image            string    `json:"image" validate:"required"`
-	Link             string    `json:"link" validate:"required"`
-	Description      string    `json:"description" validate:"required"`
-	UpdatedAt        time.Time `json:"updated_at"`
+	Image       string    `form:"image" validate:"required"`
+	Link        string    `form:"link" json:"link" validate:"required"`
+	Description string    `form:"description" json:"description" validate:"required"`
+	UpdatedAt   time.Time `form:"updated_at" json:"updated_at"`
 }
