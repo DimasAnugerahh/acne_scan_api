2c15dff69b509c170976be69a28297c5aac8c913
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-12-07 20:45:35 +0800

    feat: implement middleware

--- internal/app/articles/routes/article_routes.go
+++ internal/app/articles/routes/article_routes.go
@@ -1,11 +1,15 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"acne-scan-api/internal/middleware"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func (articleRoutes *ArticleRoutesImpl) ArticleRoutes(app *fiber.App) {
 	app.Post("/articles", articleRoutes.ArticleHandler.Create)
-	app.Get("/articles", articleRoutes.ArticleHandler.GetAll)
-	app.Get("/articles/:id", articleRoutes.ArticleHandler.GetById)
+	app.Get("/articles",middleware.UserMiddleware(), articleRoutes.ArticleHandler.GetAll)
+	app.Get("/articles/:id",middleware.UserMiddleware(), articleRoutes.ArticleHandler.GetById)
 	app.Delete("/articles/:id", articleRoutes.ArticleHandler.Delete)
 	app.Patch("/articles/:id", articleRoutes.ArticleHandler.Update)
 
--- internal/app/product_recommendation/routes/product_recommendation_routes.go
+++ internal/app/product_recommendation/routes/product_recommendation_routes.go
@@ -1,13 +1,15 @@
 package routes
 
 import (
+	"acne-scan-api/internal/middleware"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func (ar *ProductRecommendationRoutesImpl) ProductRecommendationRoutes(app *fiber.App){
 	app.Post("/recommendations",ar.productRecommendationHandler.Create)
-	app.Get("/recommendations",ar.productRecommendationHandler.GetAll)
-	app.Get("/recommendations/:id",ar.productRecommendationHandler.GetById)
+	app.Get("/recommendations",middleware.UserMiddleware(),ar.productRecommendationHandler.GetAll)
+	app.Get("/recommendations/:id",middleware.UserMiddleware(),ar.productRecommendationHandler.GetById)
 	app.Patch("/recommendations/:id",ar.productRecommendationHandler.Update)
 	app.Delete("/recommendations/:id",ar.productRecommendationHandler.Delete)
 
