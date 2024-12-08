b16be6924017a11a19dc47192d131502254b91a3
DimasAnugerahh <dimasnaugrah883@gmail.com>
2024-12-08 16:25:10 +0800

    refactor: add error message

--- internal/app/history/handlers/history_handlers_create.go
+++ internal/app/history/handlers/history_handlers_create.go
@@ -26,10 +26,13 @@ func (history *HistoryHandlersImpl) Create(c *fiber.Ctx) error {
 		log.Fatalf("Error serializing map to JSON: %v", err)
 	}
 
-	err = history.service.Create(req,historyJson)
+	err = history.service.Create(req, historyJson)
 	if err != nil {
 		if strings.Contains(err.Error(), "validation") {
 			return validation.ValidationError(c, err)
+		}
+		if strings.Contains(err.Error(), "FOREIGN KEY (`user_id`)") {
+			return response.InternalServerError(c, "Data user tidak ditemukan", err.Error())
 		}
 		return response.InternalServerError(c, "failed to create history, something happen", err.Error())
 	}
